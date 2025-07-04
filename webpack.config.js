73aa75af0850e425f0eefb773308e76273686f77
Alexander-2049 <a.alexander.2049@gmail.com>
2025-07-05 02:17:38 +0300

    feat: add webpack configuration for TypeScript project

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devServer: {
+    static: './public',
+    port: 3000,
+  },
+  mode: 'development',
+};
